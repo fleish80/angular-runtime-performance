f7f7e780478ade2bf080fb6642f72e29b0b60fa4
DXF0530 <dmitry.fleish@sbdinc.com>
2023-01-15 10:28:46 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-runtime-performance-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-runtime-performance';
+}
